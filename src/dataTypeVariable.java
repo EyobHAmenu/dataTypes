73780b4b2942c2cc1b203cff9fbddcae2a2838c7
EyobHAmenu <heyob888@gmail.com>
2020-04-14 16:18:24 -0400

    Second commit

--- src/dataTypeVariable.java
+++ src/dataTypeVariable.java
@@ -1,22 +1,20 @@
 public class dataTypeVariable {
     public static void main (String[] args)
     {
-        boolean flag =true;
-        byte b = '4';
-        char a = 'a';
-        short index = 3;
-        int i = 89;
-        float f = 4.73f;
-        long longVar = 1234567890;
-        double double_var = 123456789.123456789;
+        /*
+        Test Score initialized is: 50
+        Price tag: 24.99
+        Username: Bobby Johnson
+        */
 
-        System.out.println("Flag " + flag);
-        System.out.println("Byte " + b);
-        System.out.println("Char " + a);
-        System.out.println("short " + index);
-        System.out.println("int " + i);
-        System.out.println("float " + f);
-        System.out.println("long " + longVar);
-        System.out.println("double " + double_var);
+        int testScore = 50;
+        double price = 24.99;
+        String userName = "Bobby Johnson";
+
+
+        System.out.println("Test Score initialized is: " + testScore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + userName);
+
     }
 }
